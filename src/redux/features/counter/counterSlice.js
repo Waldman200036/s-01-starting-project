611bf4353fa55ce0a77604ebf29cb0a3e8a995f1
Walter Walden <Waldman200036@gmail.com>
2022-09-04 17:30:41 -0500

    Added Redux toolkit enhancements

--- src/redux/features/counter/counterSlice.js
+++ src/redux/features/counter/counterSlice.js
@@ -63,6 +63,9 @@ export const counterSlice = createSlice({
 export const { increment, decrement, incrementByAmount, toggleCounter } =
   counterSlice.actions;
 
+// Could also use instead and include all by default
+export const counterActions = counterSlice.actions;
+
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
