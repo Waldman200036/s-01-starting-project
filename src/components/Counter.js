c64e8ccb9b5ada1b7bd76049cc6767cc5e220a12
Walter Walden <Waldman200036@gmail.com>
2022-09-04 14:48:23 -0500

    Added hardcode add bby 5 increment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,18 +2,26 @@ import { useSelector, useDispatch } from "react-redux";
 import {
   decrement,
   increment,
+  incrementByAmount,
   selectCount,
 } from "../redux/features/counter/counterSlice";
 import classes from "./Counter.module.css";
-
+import { useState } from "react";
 export const Counter = () => {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState("5");
 
+  const incrementValue = Number(incrementAmount) || 5;
+
+  
   const incrementHandler = () => {
     dispatch(increment());
   };
 
+  const increaseHandler = ()=>{
+    dispatch(incrementByAmount(5))
+  }
   const decrementHandler = () => {
     dispatch(decrement());
   };
@@ -25,7 +33,9 @@ export const Counter = () => {
       <h1>Redux Counter</h1>
       <div className={classes.value}>{count}</div>
       <div>
+        {/* <input value={incrementAmount}></input> */}
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
